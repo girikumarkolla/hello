3dfe665c202c913c8527549f58a3f60888225337
girikumarkolla <girikumar.kolla@gmail.com>
2018-09-04 11:02:29 +0530

    deleted commits

--- df.py
+++ df.py
@@ -4,8 +4,7 @@ import playfair
 # create a Playfair object for encrypting and decrypting
 ph = playfair.Playfair()
 
-# set the password for upcoming encryptions or decryptions
 ph.setPassword('Caput Draconis')
 
-# encrypt a plain text phrase and print it out
+
 print (ph.encrypt('Emma Watson is the most talented actress.'))
